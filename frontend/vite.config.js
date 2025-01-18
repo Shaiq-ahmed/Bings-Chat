b7feb3d6600b830cb5a6dcdf177ba00f44ab8f91
Shaiq Ahmed <58372457+Shaiq-ahmed@users.noreply.github.com>
2025-01-19 01:27:40 +0500

    changes13

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,15 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: import.meta.env.VITE_BACKEND_BASE_URL, // Your backend API URL
+        changeOrigin: true,
+        secure: true,
+      },
+    },
+  },
   define: {
     global: 'globalThis',
     'process.env': {},
